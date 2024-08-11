33c7d79dc509aae873b27a2ead583d36c40d9d62
Luiz Gabriel Pessanha <140475020+LuizDev271@users.noreply.github.com>
2024-08-11 12:44:38 -0300

    Perguntas JS

--- JavaScript/perguntas.js
+++ JavaScript/perguntas.js
@@ -1,6 +1,8 @@
 const perguntas = document.getElementsByClassName('perguntas')
 const perguntasFeitas = document.getElementsByClassName('pergunta-feita')
 const perguntasTamanho = document.getElementById("perguntas")
+const rodape = document.getElementById("rodape")
+const icones2 = document.getElementsByClassName('fa-solid')
 
 perguntas[0].addEventListener("click", abrirPergunta1)
 perguntas[1].addEventListener("click", abrirPergunta2)
@@ -22,20 +24,26 @@ function abrirPergunta1(){
     if(largura3 >= 1200){
         if(aberta2[0] == false){
             perguntasFeitas[0].style.display = 'block'
-            aberta2[0] = true
 
-            if(aberta2[7] == true && aberta2[6] == true){
-                perguntasTamanho.style.height = '900px'
-            }
+            rodape.style.opacity = '0'
+            rodape.style.marginTop = '330px'
+
+            icones2[20].classList.remove('fa-caret-down')
+            icones2[20].classList.add('fa-caret-up')
+
+            aberta2[0] = true
 
         } else if(aberta2[0] == true){
             perguntasFeitas[0].style.display = 'none'
             aberta2[0] = false
 
-            if(aberta2[7] == true && aberta2[6] == true){
-                perguntasTamanho.style.height = '800px'
-            }
+            icones2[20].classList.remove('fa-caret-up')
+            icones2[20].classList.add('fa-caret-down')
 
+            if(aberta2[0] == false && aberta2[1] == false && aberta2[2] == false && aberta2[3] == false && aberta2[4] == false && aberta2[5] == false && aberta2[6] == false && aberta2[7] == false){
+                rodape.style.opacity = '1'
+                rodape.style.marginTop = '0px'
+            }
         }
     }
 }
@@ -44,20 +52,26 @@ function abrirPergunta2(){
     if(largura3 >= 1200){
         if(aberta2[1] == false){
             perguntasFeitas[1].style.display = 'block'
-            aberta2[1] = true
 
-            if(aberta2[7] == true && aberta2[6] == true){
-                perguntasTamanho.style.height = '900px'
-            }
+            rodape.style.opacity = '0'
+            rodape.style.marginTop = '330px'
+
+            icones2[21].classList.remove('fa-caret-down')
+            icones2[21].classList.add('fa-caret-up')
+
+            aberta2[1] = true
 
         } else if(aberta2[1] == true){
             perguntasFeitas[1].style.display = 'none'
             aberta2[1] = false
 
-            if(aberta2[7] == true && aberta2[6] == true){
-                perguntasTamanho.style.height = '800px'
-            }
+            icones2[21].classList.remove('fa-caret-up')
+            icones2[21].classList.add('fa-caret-down')
 
+            if(aberta2[0] == false && aberta2[1] == false && aberta2[2] == false && aberta2[3] == false && aberta2[4] == false && aberta2[5] == false && aberta2[6] == false && aberta2[7] == false){
+                rodape.style.opacity = '1'
+                rodape.style.marginTop = '0px'
+            }
         }
     }
 }
@@ -66,18 +80,26 @@ function abrirPergunta3(){
     if(largura3 >= 1200){
         if(aberta2[2] == false){
             perguntasFeitas[2].style.display = 'block'
-            aberta2[2] = true
 
-            if(aberta2[0] == true && aberta2[1] == true){
-                perguntasTamanho.style.height = '900px'
-            }
+            rodape.style.opacity = '0'
+            rodape.style.marginTop = '330px'
+
+            icones2[22].classList.remove('fa-caret-down')
+            icones2[22].classList.add('fa-caret-up')
+
+            aberta2[2] = true
 
         } else if(aberta2[2] == true){
             perguntasFeitas[2].style.display = 'none'
             aberta2[2] = false
-            
-            perguntasTamanho.style.height = '800px'
 
+            icones2[22].classList.remove('fa-caret-down')
+            icones2[22].classList.add('fa-caret-up')
+
+            if(aberta2[0] == false && aberta2[1] == false && aberta2[2] == false && aberta2[3] == false && aberta2[4] == false && aberta2[5] == false && aberta2[6] == false && aberta2[7] == false){
+                rodape.style.opacity = '1'
+                rodape.style.marginTop = '0px'
+            }
         }
     }
 }
@@ -86,22 +108,25 @@ function abrirPergunta4(){
     if(largura3 >= 1200){
         if(aberta2[3] == false){
             perguntasFeitas[3].style.display = 'block'
+
+            rodape.style.opacity = '0'
+            rodape.style.marginTop = '330px'
+
+            icones2[23].classList.remove('fa-caret-down')
+            icones2[23].classList.add('fa-caret-up')
+
             aberta2[3] = true
-
-            if(aberta2[7] == true && aberta2[6] == true){
-                perguntasTamanho.style.height = '900px'
-            }
-
-            if(aberta2[0] == true && aberta2[1] == true && aberta2[2]){
-                perguntasTamanho.style.height = '1000px'
-            }
 
         } else if(aberta2[3] == true){
             perguntasFeitas[3].style.display = 'none'
             aberta2[3] = false
 
-            if(aberta2[7] == true && aberta2[6] == true){
-                perguntasTamanho.style.height = '800px'
+            icones2[23].classList.remove('fa-caret-up')
+            icones2[23].classList.add('fa-caret-down')
+
+            if(aberta2[0] == false && aberta2[1] == false && aberta2[2] == false && aberta2[3] == false && aberta2[4] == false && aberta2[5] == false && aberta2[6] == false && aberta2[7] == false){
+                rodape.style.opacity = '1'
+                rodape.style.marginTop = '0px'
             }
         }
     }
@@ -111,22 +136,25 @@ function abrirPergunta5(){
     if(largura3 >= 1200){
         if(aberta2[4] == false){
             perguntasFeitas[4].style.display = 'block'
+
+            rodape.style.opacity = '0'
+            rodape.style.marginTop = '330px'
+
+            icones2[24].classList.remove('fa-caret-down')
+            icones2[24].classList.add('fa-caret-up')
+
             aberta2[4] = true
-
-            if(aberta2[0] == true && aberta2[1] == true && aberta2[2] && aberta2[3]){
-                perguntasTamanho.style.height = '1050px'
-            }
-
-            if(aberta2[7] == true && aberta2[6] == true){
-                perguntasTamanho.style.height = '900px'
-            }
 
         } else if(aberta2[4] == true){
             perguntasFeitas[4].style.display = 'none'
             aberta2[4] = false
 
-            if(aberta2[7] == true && aberta2[6] == true){
-                perguntasTamanho.style.height = '800px'
+            icones2[24].classList.remove('fa-caret-up')
+            icones2[24].classList.add('fa-caret-down')
+
+            if(aberta2[0] == false && aberta2[1] == false && aberta2[2] == false && aberta2[3] == false && aberta2[4] == false && aberta2[5] == false && aberta2[6] == false && aberta2[7] == false){
+                rodape.style.opacity = '1'
+                rodape.style.marginTop = '0px'
             }
         }
     }
@@ -136,30 +164,25 @@ function abrirPergunta6(){
     if(largura3 >= 1200){
         if(aberta2[5] == false){
             perguntasFeitas[5].style.display = 'block'
+
+            rodape.style.opacity = '0'
+            rodape.style.marginTop = '330px'
+
+            icones2[25].classList.remove('fa-caret-down')
+            icones2[25].classList.add('fa-caret-up')
+
             aberta2[5] = true
-
-            if(aberta2[0] == true && aberta2[1] == true){
-                perguntasTamanho.style.height = '900px'
-            }
-
-            if(aberta2[7] == true && aberta2[6] == true){
-                perguntasTamanho.style.height = '900px'
-            }
-
-            if((aberta2[0] == true && aberta2[1] && aberta2[2] && aberta2[3] && aberta2 [4]) || (aberta2[3] && aberta2[4])){
-                perguntasTamanho.style.height = '1100px'
-            }
 
         } else if(aberta2[5] == true){
             perguntasFeitas[5].style.display = 'none'
             aberta2[5] = false
-            
-            if(aberta2[0] == true && aberta2[1] == true && aberta2[2] == true && aberta2[3] == true){
-                perguntasTamanho.style.height = '1050px'
-            }
 
-            if(aberta2[7] == true && aberta2[6] == true){
-                perguntasTamanho.style.height = '800px'
+            icones2[25].classList.remove('fa-caret-up')
+            icones2[25].classList.add('fa-caret-down')
+
+            if(aberta2[0] == false && aberta2[1] == false && aberta2[2] == false && aberta2[3] == false && aberta2[4] == false && aberta2[5] == false && aberta2[6] == false && aberta2[7] == false){
+                rodape.style.opacity = '1'
+                rodape.style.marginTop = '0px'
             }
         }
     }
@@ -169,28 +192,26 @@ function abrirPergunta7(){
     if(largura3 >= 1200){
         if(aberta2[6] == false){
             perguntasFeitas[6].style.display = 'block'
+
+            rodape.style.opacity = '0'
+            rodape.style.marginTop = '330px'
+
+            icones2[26].classList.remove('fa-caret-down')
+            icones2[26].classList.add('fa-caret-up')
+
             aberta2[6] = true
-
-            if(aberta2[0] == true && aberta2[1] == true){
-                perguntasTamanho.style.height = '900px'
-            }
-
-            if(aberta2[0] == true && aberta2[1] == true && aberta2[2] == true && aberta2[3] == true && aberta2[4] == true && aberta2[5]){
-                perguntasTamanho.style.height = '1175px'
-            }
 
         } else if(aberta2[6] == true){
             perguntasFeitas[6].style.display = 'none'
             aberta2[6] = false
 
-            if(aberta2[0] == true && aberta2[1] == true){
-                perguntasTamanho.style.height = '850px'
-            }
+            icones2[26].classList.remove('fa-caret-up')
+            icones2[26].classList.add('fa-caret-down')
 
-            if(aberta2[0] == true && aberta2[1] == true && aberta2[2] == true && aberta2[3] == true && aberta2[4] == true && aberta2[5]){
-                perguntasTamanho.style.height = '1100px'
+            if(aberta2[0] == false && aberta2[1] == false && aberta2[2] == false && aberta2[3] == false && aberta2[4] == false && aberta2[5] == false && aberta2[6] == false && aberta2[7] == false){
+                rodape.style.opacity = '1'
+                rodape.style.marginTop = '0px'
             }
-
         }
     }
 }
@@ -199,34 +220,25 @@ function abrirPergunta8(){
     if(largura3 >= 1200){
         if(aberta2[7] == false){
             perguntasFeitas[7].style.display = 'block'
+
+            rodape.style.opacity = '0'
+            rodape.style.marginTop = '330px'
+
+            icones2[27].classList.remove('fa-caret-down')
+            icones2[27].classList.add('fa-caret-up')
+
             aberta2[7] = true
-
-            if(aberta2[0] == true && aberta2[1] == true){
-                perguntasTamanho.style.height = '850px'
-            }
-
-            if(aberta2[0] == true && aberta2[1] == true && aberta2[6] == true){
-                perguntasTamanho.style.height = '925px'
-            }
-
-            if(aberta2[0] == true && aberta2[1] == true && aberta2[2] == true && aberta2[3] == true && aberta2 [4]){
-                perguntasTamanho.style.height = '925px'
-            }
-
-            if(aberta2[0] == true && aberta2[1] == true && aberta2[2] == true && aberta2[3] == true && aberta2 [4] == true && aberta2[5] == true && aberta2[6] == true){
-                perguntasTamanho.style.height = '1225px'
-            }
 
         } else if(aberta2[7] == true){
             perguntasFeitas[7].style.display = 'none'
             aberta2[7] = false
-            
-            if(aberta2[0] == true && aberta2[1] == true){
-                perguntasTamanho.style.height = '800px'
-            }
 
-            if(aberta2[0] == true && aberta2[1] == true && aberta2[6] == true){
-                perguntasTamanho.style.height = '925px'
+            icones2[27].classList.remove('fa-caret-up')
+            icones2[27].classList.add('fa-caret-down')
+
+            if(aberta2[0] == false && aberta2[1] == false && aberta2[2] == false && aberta2[3] == false && aberta2[4] == false && aberta2[5] == false && aberta2[6] == false && aberta2[7] == false){
+                rodape.style.opacity = '1'
+                rodape.style.marginTop = '0px'
             }
         }
     }
